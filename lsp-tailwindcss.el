a6e1990eda7763e1f8c144432f6ecda09a8f1128
A.I <merrick@luois.me>
2020-06-19 23:39:56 +0800

    add function to install language server

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -26,26 +26,60 @@
   "lsp support for tailwind css"
   :group 'lsp-mode)
 
-;; (defcustom lsp-tailwindcss-download-location nil)
-
-(defcustom lsp-tailwindcss-server-file "index.js"
-  "index.js location of vscode-tailwindcss"
+(defcustom lsp-tailwindcss-server-download-location
+  (expand-file-name "vscode-tailwindcss" user-emacs-directory)
+  "download location for automatic install"
+  'type 'string
   :group 'lsp-tailwindcss)
+
+(defcustom lsp-tailwindcss-server-file
+  (expand-file-name "dist/server/index.js" lsp-tailwindcss-server-download-location)
+  "index.js location of vscode-tailwindcss"
+  :type 'string
+  :group 'lsp-tailwindcss)
+
+(defcustom lsp-tailwindcss-server-remote
+  "https://github.com/bradlc/vscode-tailwindcss"
+  "git repo of tailwindcss language server"
+  :type 'string
+  :group 'lsp-tailwindcss)
+
+(defvar lsp-tailwindcss-server-installed-p
+  (file-exists-p lsp-tailwindcss-server-file)
+  "check if server is installed")
 
 (defun lsp-tailwindcss--callback (workspace &rest _)
   (message "lsp-tailwindcss callback %s" workspace))
 
+(defun lsp-tailwindcss-install-server ()
+  (interactive)
+  (if lsp-tailwindcss-server-installed-p
+      (message "tailwindcss language server already installed")
+    (let ((remote lsp-tailwindcss-server-remote)
+          (local lsp-tailwindcss-server-download-location)
+          (call #'lsp-tailwindcss--call-process))
+      (message "installing vscode-tailwindcss, please wait.")
+      (funcall call "git" "clone" remote local)
+      (message "building tailwindcss lsp server")
+      (let ((default-directory local))
+        (funcall call "npm" "install")
+        (funcall call "npm" "run" "build")
+        (message "tailwindcss language server installed")))))
+
+(defun lsp-tailwindcss--call-process (command &rest args)
+  (with-temp-buffer
+    (cons (or (apply #'call-process command nil t nil (remq nil args))
+              -1)
+          (string-trim (buffer-string)))))
+
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection
-                   '("node" lsp-tailwindcss-server-file "--stdio")
+                   (list "node" lsp-tailwindcss-server-file "--stdio")
                    (lambda () (f-exists? lsp-tailwindcss-server-file)))
   :major-modes '(web-mode css-mode)
   :server-id 'tailwindcss
   :priority 1
   :notification-handlers (lsp-ht ("tailwindcss/configUpdated" 'lsp-tailwindcss--callback))))
-
-;; (add-hook 'web-mode-hook #'lsp)
-;; (add-hook 'css-mode-hook #'lsp)
 
 (provide 'lsp-tailwindcss)
