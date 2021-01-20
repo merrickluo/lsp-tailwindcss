60c6ce56a26e548648849402784d2b7d5f318c9c
A.I <merrick@luois.me>
2021-01-20 18:38:35 +0800

    cleanup and add comments

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -1,9 +1,13 @@
-;;; lsp-tailwindcss.el --- the lsp-mode client for tailwindcss  -*- lexical-binding: t; -*-
+;;; lsp-tailwindcss.el --- A lsp-mode client for tailwindcss  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020  A.I.
 
 ;; Author: A.I. <merrick@luois.me>
 ;; Keywords: language tools
+;; Version: 0.1
+;; Package-Requires: ((lsp-mode "3.0") (emacs "24.3"))
+;; Keywords: tailwindcss
+;; URL: https://github.com/merrickluo/lsp-tailwindcss
 
 ;; This file is not part of GNU Emacs
 
@@ -20,6 +24,11 @@
 ;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; provide the connection to lsp-mode and tailwindcss language server
+
+;;; Code:
 (require 'lsp-mode)
 
 (defgroup lsp-tailwindcss nil
@@ -53,18 +62,16 @@
   (file-exists-p lsp-tailwindcss-server-file)
   "check if server is installed")
 
-(defvar-local vscode-tailwindcss-version "0.5.6")
-
 (defun lsp-tailwindcss--download-url ()
   (let ((version lsp-tailwindcss-server-version))
     (format "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%s/vscode-tailwindcss-%s.vsix"
             version version)))
 
-(defun lsp-tailwindcss--callback (workspace &rest args)
+(defun lsp-tailwindcss--callback (_workspace &rest _args)
   ;; no action needed right now
   )
 
-(defun lsp-tailwindcss--install-server (client callback error-callback update?)
+(defun lsp-tailwindcss--install-server (_client callback error-callback update?)
   (if (and (not update?) lsp-tailwindcss-server-installed-p)
       (lsp--info "tailwindcss language server already installed.")
     (let ((tempfile (make-temp-file "ext" nil ".zip")))
@@ -81,7 +88,7 @@
        :url (lsp-tailwindcss--download-url)
        :store-path tempfile))))
 
-(defun lsp-tailwindcss--configuration (workspace args)
+(defun lsp-tailwindcss--configuration (_workspace args)
   (let ((id (gethash "_id" args)))
     (lsp-request "tailwindcss/getConfigurationResponse" `(:_id ,id) :no-wait t)))
 
@@ -101,3 +108,4 @@
                           (lsp-tailwindcss--install-server client callback error-callback update?)))))
 
 (provide 'lsp-tailwindcss)
+;;; lsp-tailwindcss.el ends here
