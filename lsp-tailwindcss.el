f37767cd0abf1aca548d5183bc88b34390e36da6
A.I <merrick@luois.me>
2021-08-16 22:25:00 +0800

    remove unused code

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -5,7 +5,7 @@
 ;; Author: A.I. <merrick@luois.me>
 ;; Keywords: language tools
 ;; Version: 0.2
-;; Package-Requires: ((lsp-mode "3.0") (emacs "24.3"))
+;; Package-Requires: ((lsp-mode "7.1") (emacs "24.3"))
 ;; Keywords: tailwindcss
 ;; URL: https://github.com/merrickluo/lsp-tailwindcss
 
@@ -35,26 +35,6 @@
   "lsp support for tailwind css"
   :group 'lsp-mode)
 
-(defcustom lsp-tailwindcss-server-dir (expand-file-name "tailwindcss" lsp-server-install-dir)
-  "Local directory for tailwindcss-intellisense."
-  :type 'string
-  :group 'lsp-tailwindcss)
-
-(defcustom lsp-tailwindcss-server-file (expand-file-name "extension/dist/server/tailwindServer.js" lsp-tailwindcss-server-dir)
-  "The index.js file location of tailwindcss-intellisense, do not change when auto install."
-  :type 'string
-  :group 'lsp-tailwindcss)
-
-(defcustom lsp-tailwindcss-auto-install-server t
-  "Install tailwindcss language server automatically."
-  :type 'boolean
-  :group 'lsp-tailwindcss)
-
-(defcustom lsp-tailwindcss-server-version "0.6.13"
-  "Specify the version of tailwindcss intellisence."
-  :type 'string
-  :group 'lsp-tailwindcss)
-
 (defcustom lsp-tailwindcss-add-on-mode nil
   "Specify lsp-tailwindcss as add-on so it can work with other language servers."
   :type 'boolean
@@ -72,57 +52,25 @@
                   :package "@tailwindcss/language-server"
                   :path "tailwindcss-language-server"))
 
-(defvar lsp-tailwindcss-server-installed-p
-  (file-exists-p lsp-tailwindcss-server-file)
-  "Check if server is installed.")
-
-(defun lsp-tailwindcss--download-url ()
-  "Build langauge server download url from version."
-  (let ((version lsp-tailwindcss-server-version))
-    (format "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%s/vscode-tailwindcss-%s.vsix"
-            version version)))
-
-(defun lsp-tailwindcss--callback (_workspace &rest _args)
-  "No action needed right now.")
-
-(defun lsp-tailwindcss--install-server (_client callback error-callback update?)
-  "Install tailwindcss langauge server.
-Required argument CALLBACK callback after install successfully.
-Required argument ERROR-CALLBACK callback when error occurs during installation.
-Required argument UPDATE? Indicates if this is called for update."
-  (if (and (not update?) lsp-tailwindcss-server-installed-p)
-      (lsp--info "tailwindcss language server already installed.")
-    (let ((tempfile (make-temp-file "ext" nil ".zip")))
-      (lsp--info "installing tailwindcss language server, please wait.")
-      (delete-file tempfile)
-      (lsp-download-install
-       (lambda (&rest _)
-         (condition-case err
-             (progn
-               (lsp-unzip tempfile lsp-tailwindcss-server-dir)
-               (funcall callback))
-           (error (funcall error-callback err))))
-       error-callback
-       :url (lsp-tailwindcss--download-url)
-       :store-path tempfile))))
-
-(defun lsp-tailwindcss--configuration (_workspace args)
-  "Respond to langauge server with empty configuration.
-Required argument ARGS Arguments from the language server."
-  (ht-merge
-   (lsp-configuration-section "tailwindcss")
-   (ht ("editor.tabSize" (symbol-value (lsp--get-indent-width major-mode))))))
-
-(defun lsp-tailwindcss--initialization-options ()
-  (ht ("configuration" (lsp-configuration-section "tailwindcss"))
-      ("userLanguages" (ht))))
-
 (defun lsp-tailwindcss--activate-p (&rest _args)
   (and (lsp-workspace-root)
        (or (file-exists-p (f-join (lsp-workspace-root) "tailwind.config.js"))
            (file-exists-p (f-join (lsp-workspace-root) "assets" "tailwind.config.js"))
            (locate-dominating-file (buffer-file-name) "tailwind.config.js"))
        (apply #'provided-mode-derived-p major-mode lsp-tailwindcss-major-modes)))
+
+(defun lsp-tailwindcss--company-dash-hack (w)
+  (with-lsp-workspace w
+    (let* ((caps (lsp--workspace-server-capabilities w))
+           (comp (lsp:server-capabilities-completion-provider? caps))
+           (trigger-chars (append (lsp:completion-options-trigger-characters? comp) nil)))
+      (lsp:set-completion-options-trigger-characters?
+       comp
+       (vconcat
+        (cl-pushnew "-" trigger-chars :test #'string=))))))
+
+(defun lsp-tailwindcss--initialization-options ()
+  (ht ("configuration" (lsp-configuration-section "tailwindcss"))))
 
 (lsp-register-client
  (make-lsp-client
@@ -134,15 +82,7 @@ Required argument ARGS Arguments from the language server."
   :priority -1
   :add-on? lsp-tailwindcss-add-on-mode
   :initialization-options #'lsp-tailwindcss--initialization-options
-  :initialized-fn (lambda (w)
-                    (with-lsp-workspace w
-                      (let* ((caps (lsp--workspace-server-capabilities w))
-                             (comp (lsp:server-capabilities-completion-provider? caps))
-                             (trigger-chars (append (lsp:completion-options-trigger-characters? comp) nil)))
-                        (lsp:set-completion-options-trigger-characters?
-                         comp
-                         (vconcat
-                          (cl-pushnew "-" trigger-chars :test #'string=))))))
+  :initialized-fn #'lsp-tailwindcss--company-dash-hack
   :download-server-fn (lambda (_client callback error-callback _update?)
                         (when lsp-tailwindcss-auto-install-server
                           (lsp-package-ensure 'tailwindcss-language-server callback error-callback)))))
