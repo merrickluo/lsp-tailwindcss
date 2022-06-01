09377d5bbb742f11c458481905ddbb022dd2b0da
A.I <merrick@luois.me>
2022-06-01 23:09:47 +0800

    better configuration, please reinstall server after update (#43)
    * update docstring to make linter happy
    
    * match server's config file detect logic
    
    - find `tailwindcss.config.js` and `tailwindcss.config.cjs` in workspace
    - declare `f` as dependency
    
    * add new experimental config
    
    tailwindCSS.experimental.configFile
    
    * fix not matching $wsroot/tailwind.config.js
    
    * update README
    
    * fix eask stuff

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -5,7 +5,7 @@
 ;; Author: A.I. <merrick@luois.me>
 ;; Keywords: language tools
 ;; Version: 0.2
-;; Package-Requires: ((lsp-mode "7.1") (emacs "26.1"))
+;; Package-Requires: ((lsp-mode "7.1") (f "0.20.0") (emacs "26.1"))
 ;; Keywords: tailwindcss
 ;; URL: https://github.com/merrickluo/lsp-tailwindcss
 
@@ -32,7 +32,7 @@
 (require 'lsp-mode)
 
 (defgroup lsp-tailwindcss nil
-  "lsp support for tailwind css"
+  "Adds lsp-mode support for tailwindcss."
   :group 'lsp-mode)
 
 (defcustom lsp-tailwindcss-add-on-mode nil
@@ -46,7 +46,7 @@
   :group 'lsp-tailwindcss)
 
 (defcustom lsp-tailwindcss-major-modes '(rjsx-mode web-mode html-mode css-mode typescript-mode)
-  "Specify lsp-tailwindcss should only starts when major-mode in the list or derived from them."
+  "Major modes that lsp-tailwindcss should activate."
   :type 'list
   :group 'lsp-tailwindcss
   :package-version '(lsp-tailwindcss . "0.2"))
@@ -54,7 +54,16 @@
 (defcustom lsp-tailwindcss-rustywind-command (executable-find "rustywind")
   "[Experimental] Command use for sort the tailwindcss classes."
   :type 'string
-  :group 'lsp-tailwindcss)
+  :group 'lsp-tailwindcss
+  :package-version '(lsp-tailwindcss . "0.3"))
+
+(defcustom lsp-tailwindcss-skip-config-check nil
+  "Force skip config file check.
+Only use it when your config file are in unconventional location
+and make sure tailwindcss language server can find it."
+  :type 'boolean
+  :group 'lsp-tailwindcss
+  :package-version '(lsp-tailwindcss . "0.3"))
 
 (defun lsp-tailwindcss-rustywind ()
   "[Experimental] Sort tailwindcss class name using rustywind.
@@ -79,18 +88,18 @@ This is an *experimental* feature, please be careful when use."
     (error (format "Can't find rustywind executable at %s" lsp-tailwindcss-rustywind-command))))
 
 (defun lsp-tailwindcss-rustywind-before-save()
-  "[Experimental] Add this to `before-save-hook' to run rustywind when saving buffer.
-(add-hook 'before-save-hook #'lsp-tailwindcss-rustywind-before-save).
-
-It only runs when lsp-tailwindcss can be activated determined by `lsp-tailwindcss--activate-p'.
-This is an *experimental* feature, please be careful when use.
-"
+  "[Experimental] Run rustywind when saving buffer.
+By add this to `before-save-hook',
+it only runs when lsp-tailwindcss can be activated,
+see `lsp-tailwindcss--activate-p'.
+This is an *experimental* feature, please use it carefully."
   (when (lsp-tailwindcss--activate-p)
     (lsp-tailwindcss-rustywind)))
 
 ;;; Language server global settings:
 (defcustom lsp-tailwindcss-emmet-completions nil
-  "Enable completions when using Emmet-style syntax, for example div.bg-red-500.uppercase."
+  "Enable completions when using Emmet-style syntax.
+For example div.bg-red-500.uppercase."
   :type 'boolean
   :group 'lsp-tailwindcss
   :package-version '(lsp-tailwindcss . "0.2"))
@@ -102,17 +111,19 @@ This is an *experimental* feature, please be careful when use.
   :package-version '(lsp-tailwindcss . "0.2"))
 
 (defcustom lsp-tailwindcss-root-font-size 16
- "Root font size in pixels. Used to convert rem CSS values to their px equivalents.
-see `lsp-tailwindcss-show-pixel-equivalents'"
+ "Root font size in pixels.
+Used to convert rem CSS values to their px equivalents,
+ see `lsp-tailwindcss-show-pixel-equivalents'"
   :type 'number
   :group 'lsp-tailwindcss
   :package-version '(lsp-tailwindcss . "0.2"))
 
 (defcustom lsp-tailwindcss-validate t
-  "Enable linting. Rules can be configured individually using the lsp-tailwindcss-lint-* settings:
-    ignore: disable lint rule entirely
-    warning: rule violations will be considered \"warnings\", typically represented by a yellow underline
-    error: rule violations will be considered \"errors\", typically represented by a red underline."
+  "Enable linting.
+Rules can be configured individually using the lsp-tailwindcss-lint-* settings:
+  ignore: disable lint rule entirely
+  warning: rule violations will be considered \"warnings\"
+  error: rule violations will be considered \"errors\""
   :type 'boolean
   :group 'lsp-tailwindcss
   :package-version '(lsp-tailwindcss . "0.2"))
@@ -176,7 +187,8 @@ see `lsp-tailwindcss-show-pixel-equivalents'"
   :package-version '(lsp-tailwindcss . "0.2"))
 
 (defcustom lsp-tailwindcss-lint-css-conflict "warning"
-  "Class names on the same HTML element which apply the same CSS property or properties."
+  "Class names on the same HTML element.
+Which apply the same CSS property or properties."
   :type '(choice (const "ignore")
                  (const "warning")
                  (const "error"))
@@ -200,8 +212,23 @@ This is a undocumented setting, see https://github.com/tailwindlabs/tailwindcss-
   :package-version '(lsp-tailwindcss . "0.3"))
 
 (defcustom lsp-tailwindcss-class-attributes ["class" "className" "ngClass"]
-  "The HTML attributes for which to provide class completions, hover previews, linting etc."
+  "The HTML attributes to provide class completions, hover previews, linting etc."
   :type 'lsp-string-vector
+  :group 'lsp-tailwindcss
+  :package-version '(lsp-tailwindcss . "0.3"))
+
+(defcustom lsp-tailwindcss-experimental-config-file nil
+  "Manually specify the Tailwind config file or files.
+It can be a single string or a hash map. see
+https://github.com/tailwindlabs/tailwindcss-intellisense#tailwindcssexperimentalconfigfile
+Example:
+\(setq lsp-tailwindcss-experimental-config-file \"config/tailwindcss.conf.js\")
+
+\(setq lsp-tailwindcss-experimental-config-file
+      (ht
+       (\"themes/simple/tailwind.config.js\" \"themes/simple/**\")
+       (\"themes/neon/tailwind.config.js\" \"themes/neon/**\")))"
+  :type 'other
   :group 'lsp-tailwindcss
   :package-version '(lsp-tailwindcss . "0.3"))
 
@@ -221,6 +248,7 @@ This is a undocumented setting, see https://github.com/tailwindlabs/tailwindcss-
    ("tailwindCSS.lint.cssConflict" lsp-tailwindcss-lint-css-conflict)
    ("tailwindCSS.lint.recommendedVariantOrder" lsp-tailwindcss-lint-recommended-variant-order)
    ("tailwindCSS.experimental.classRegex" lsp-tailwindcss-experimental-class-regex)
+   ("tailwindCSS.experimental.configFile" lsp-tailwindcss-experimental-config-file)
    ("tailwindCSS.classAttributes" lsp-tailwindcss-class-attributes)))
 ;;; Language server global settings ends here
 
@@ -242,22 +270,34 @@ When installed from the vscode extension."
                   :store-path ,(f-join lsp-server-install-dir "tailwindcss" "server")
                   :binary-path lsp-tailwindcss-server-command))
 
+(defun lsp-tailwindcss--has-config-file ()
+  "Check if there is a tailwindcss config file exists.
+To keep it simple and performant,only check for conventional location.
+see `lsp-tailwindcss-skip-config-check'"
+  (or lsp-tailwindcss-skip-config-check
+      lsp-tailwindcss-experimental-config-file
+      (file-exists-p (f-join (lsp-workspace-root) "tailwind.config.js"))
+      (file-exists-p (f-join (lsp-workspace-root) "config" "tailwind.config.js"))
+      (file-exists-p (f-join (lsp-workspace-root) "assets" "tailwind.config.js"))
+      (locate-dominating-file (buffer-file-name) "tailwind.config.js")
+      (file-exists-p (f-join (lsp-workspace-root) "tailwind.config.cjs"))
+      (file-exists-p (f-join (lsp-workspace-root) "config" "tailwind.config.js"))
+      (file-exists-p (f-join (lsp-workspace-root) "assets" "tailwind.config.cjs"))
+      (locate-dominating-file (buffer-file-name) "tailwind.config.cjs")))
+
 (defun lsp-tailwindcss--activate-p (&rest _args)
+  "Check if tailwindcss language server can/should start."
   (and (lsp-workspace-root)
        (apply #'provided-mode-derived-p major-mode lsp-tailwindcss-major-modes)
-       (or (file-exists-p (f-join (lsp-workspace-root) "tailwind.config.js"))
-           (file-exists-p (f-join (lsp-workspace-root) "config" "tailwind.config.js"))
-           (file-exists-p (f-join (lsp-workspace-root) "assets" "tailwind.config.js"))
-           (locate-dominating-file (buffer-file-name) "tailwind.config.js")
-           (file-exists-p (f-join (lsp-workspace-root) "tailwind.config.cjs"))
-           (file-exists-p (f-join (lsp-workspace-root) "assets" "tailwind.config.cjs"))
-           (locate-dominating-file (buffer-file-name) "tailwind.config.cjs"))))
+       (lsp-tailwindcss--has-config-file)))
 
-(defun lsp-tailwindcss--company-dash-hack (w)
-  "Append - to the lsp completion-trigger-characters,
-workaround the problem that company-mode completion not work when typing \"-\" in classname."
-  (with-lsp-workspace w
-    (let* ((caps (lsp--workspace-server-capabilities w))
+(defun lsp-tailwindcss--company-dash-hack (workspace)
+  "Append - to the lsp completion-trigger-characters.
+WORKSPACE: current workspace.
+workaround the problem that company-mode completion
+not work when typing \"-\" in classname."
+  (with-lsp-workspace workspace
+    (let* ((caps (lsp--workspace-server-capabilities workspace))
            (comp (lsp:server-capabilities-completion-provider? caps))
            (trigger-chars (append (lsp:completion-options-trigger-characters? comp) nil)))
       (lsp:set-completion-options-trigger-characters?
@@ -266,7 +306,7 @@ workaround the problem that company-mode completion not work when typing \"-\" i
         (cl-pushnew "-" trigger-chars :test #'string=))))))
 
 (defun lsp-tailwindcss--initialization-options ()
-  "tailwindcss-language-server requires configuration not be null."
+  "The tailwindcss-language-server requires configuration not be null."
   (ht ("configuration" (lsp-configuration-section "tailwindcss"))))
 
 (lsp-register-client
