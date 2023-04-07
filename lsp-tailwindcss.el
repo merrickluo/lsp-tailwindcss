3e57f4142c6be0f0bd7d856a9a7b18b38ca1a372
Ikapo <63411550+ikapo@users.noreply.github.com>
2023-04-07 12:51:01 +0300

    bump server to 0.9.11 (#57)

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -40,7 +40,7 @@
   :type 'boolean
   :group 'lsp-tailwindcss)
 
-(defcustom lsp-tailwindcss-server-version "0.9.7"
+(defcustom lsp-tailwindcss-server-version "0.9.11"
   "Specify the version of tailwindcss intellisence."
   :type 'string
   :group 'lsp-tailwindcss)
