744eeef6c24b1296f5720c9513657a456f9d2ae3
Merrick Luo <merrick@luois.me>
2024-10-17 23:14:42 +0800

    chore: bump default server version

--- lsp-tailwindcss.el
+++ lsp-tailwindcss.el
@@ -40,7 +40,7 @@
   :type 'boolean
   :group 'lsp-tailwindcss)
 
-(defcustom lsp-tailwindcss-server-version "0.10.2"
+(defcustom lsp-tailwindcss-server-version "0.12.6"
   "Specify the version of tailwindcss intellisence."
   :type 'string
   :group 'lsp-tailwindcss)
